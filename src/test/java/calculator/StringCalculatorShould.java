bbec9226b27690afd7c76f13159553b00665cbb4
unknown <narkhedelaxman1@gmail.com>
2021-10-17 16:10:18 +0530

    commit the changes

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,36 @@
 package calculator;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class StringCalculatorShould {
-
     @Test
-    void empty_string_should_return_0() {
+    void empty_string_should_return_0() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
-
     @Test
-    void string_with_single_number_should_return_number_as_int() {
+    void string_with_single_number_should_return_number_as_int() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
+    }
+    @Test
+    void string_with_single_number_should_return_number_as_int1() throws Exception {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("1,2"));
+    }
+    @Test
+    void string_with_single_number_should_return_number_as_int2() throws Exception {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(0, stringCalculator.add("1,2,3,4,5,6,7,8,9,10"));
+    }
+    @Test
+    void string_with_single_number_should_return_number_as_int3() throws Exception {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("1-2"));
+    }
+    @Test
+    void string_with_single_number_should_return_number_as_int4() throws Exception {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(-1, stringCalculator.add("-1"));
     }
 }
